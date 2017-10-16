0a7d17592b382bd1ecb7e769479fcfc1837326ba
tamino-martinius <dev@zaku.eu>
2017-10-16 17:52:59 +0200

    Added delete method

--- src/next_model.ts
+++ src/next_model.ts
@@ -701,6 +701,10 @@ export function Model(model: typeof NextModel): typeof NextModel {
       });
     }
 
+    delete(): Promise<NextModel> {
+      return this.model.dbConnector.save(this);
+    }
+
     get model(): typeof NextModel {
       return <typeof NextModel>this.constructor;
     }
