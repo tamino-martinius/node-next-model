7563c6d4a101919b148cd2e376535a776f853f03
tamino-martinius <dev@zaku.eu>
2017-10-26 19:56:04 +0200

    Moved typings of tests to seperate file

---
+++ src/__tests__/types.ts
@@ -0,0 +1,15 @@
+export interface Context {
+  definitions: () => void;
+  tests: () => void;
+  reset?: () => void,
+};
+
+export const context = (description: string, {definitions, tests, reset}: Context) => {
+  describe(description, () => {
+    beforeEach(definitions);
+    tests();
+    if (reset !== undefined) {
+      afterEach(reset);
+    }
+  });
+};
