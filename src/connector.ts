b9a237b1a96cf840c0720c210d7f1803491fa0cc
tamino-martinius <dev@zaku.eu>
2017-11-07 19:28:17 +0100

    Removed lodash in favor of own implementation

--- src/connector.ts
+++ src/connector.ts
@@ -4,35 +4,6 @@ import {
   Query,
 } from './next_model';
 
-import {
-//   assign,
-//   camelCase,
-//   concat,
-//   difference,
-  filter,
-//   first,
-//   flatten,
-//   includes,
-intersection,
-//   isArray,
-//   isFunction,
-//   isNil,
-//   isNumber,
-//   isObject,
-//   isString,
-//   keys,
-//   last,
-//   map,
-//   mapValues,
-//   omit,
-//   pick,
-//   snakeCase,
-  union,
-//   upperFirst,
-//   values,
-  without,
-} from 'lodash';
-
 export interface Connector {
   all(model: typeof NextModel): Promise<NextModel[]>;
   first(model: typeof NextModel): Promise<NextModel | undefined>;
@@ -49,11 +20,15 @@ export interface Storage {
   [modelName: string]: Attributes[];
 }
 
+export interface AttributesDict {
+  [id: string]: Attributes;
+}
+
 export interface IdStorage {
   [modelName: string]: number;
 }
 
-export type SpecialFilter = (items: Attributes[], query: Query) => Attributes[];
+export type SpecialFilter = (items: Attributes[], query: Query, identifier: string) => Attributes[];
 export interface SpecialFilters {
   [key: string]: SpecialFilter;
 };
@@ -67,27 +42,25 @@ export class DefaultConnector implements Connector {
     this.ids = {};
   }
 
-  private get specialFilters(): SpecialFilters {
-    const context = this;
+  private static get specialFilters(): SpecialFilters {
     return {
-      $and(items: Attributes[], predicates: Query[]): Attributes[] {
-        const arrays: Attributes[][] = predicates.map(query => context.query(items, query));
-        return intersection(...arrays);
+      $and(items: Attributes[], predicates: Query[], identifier: string): Attributes[] {
+        const arrays: Attributes[][] = predicates.map(query => DefaultConnector.query(items, query, identifier));
+        return DefaultConnector.intersection(arrays, identifier);
       },
-      $or(items: Attributes[], predicates: Query[]): Attributes[] {
-        const arrays: Attributes[][] = predicates.map(query => context.query(items, query));
-        return union(...arrays);
+      $or(items: Attributes[], predicates: Query[], identifier: string): Attributes[] {
+        const arrays: Attributes[][] = predicates.map(query => DefaultConnector.query(items, query, identifier));
+        return DefaultConnector.union(arrays, identifier);
       },
-      $not(items: Attributes[], predicates: Query[]): Attributes[] {
-        const arrays: Attributes[][] = predicates.map(query => context.query(items, query));
-        const array: Attributes[] = union(...arrays);
-        return without(items, ...array);
+      $not(items: Attributes[], predicates: Query[], identifier: string): Attributes[] {
+        const arrays: Attributes[][] = predicates.map(query => DefaultConnector.query(items, query, identifier));
+        const array: Attributes[] = DefaultConnector.union(arrays, identifier);
+        return DefaultConnector.without(items, array, identifier);
       },
     };
   };
 
-
-  private query(items: Attributes[], query: Query): Attributes[] {
+  private static query(items: Attributes[], query: Query, identifier: string): Attributes[] {
     const predicateKeys: string[] = [];
     const predicate: Query = {};
     const specialQueryKeys: string[] = [];
@@ -109,21 +82,75 @@ export class DefaultConnector implements Connector {
       if (specialQueryKeys.length > 0) {
         return this.query(items, {
           $and: [predicate, specialQuery],
-        });
+        }, identifier);
       } else {
-        return filter(items, predicate);
+        return this.filter(items, predicate);
       }
     } else {
       if (specialQueryKeys.length > 1) {
         return this.query(items, {
           $and: specialQueries,
-        });
+        }, identifier);
       } else if (specialQueryKeys[0] !== undefined) {
-        return this.specialFilters[specialQueryKeys[0]](items, query[specialQueryKeys[0]]);
+        return this.specialFilters[specialQueryKeys[0]](items, query[specialQueryKeys[0]], identifier);
       } else {
         return items;
       }
     }
+  }
+
+  private static filter(items: Attributes[], query: Query): Attributes[] {
+    return items.filter(item => {
+      for (const key in query) {
+        if (item[key] !== query[key]) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
+
+  private static intersection(attrArrays: Attributes[][], identifier: string): Attributes[]{
+    if (attrArrays.length === 0) {
+      return [];
+    } else if (attrArrays.length === 1) {
+      return attrArrays[0];
+    } else {
+      return attrArrays[0].filter(item => {
+        for (let arrayIndex = 1; arrayIndex < attrArrays.length; arrayIndex++) {
+          const found: boolean = attrArrays[arrayIndex].reduce(
+            (found: boolean, attrs: Attributes) =>
+              found || attrs[identifier] === item[identifier]
+            , false
+          );
+          if (!found) return false;
+        }
+        return true;
+      });
+    }
+  }
+
+  private static union(attrArrays: Attributes[][], identifier: string): Attributes[] {
+    if (attrArrays.length === 0) {
+      return [];
+    } else if (attrArrays.length === 1) {
+      return attrArrays[0];
+    } else {
+      const resultDict: AttributesDict = {};
+      for (const attrArray of attrArrays) {
+        for (const attrs of attrArray) {
+          resultDict[attrs[identifier].toString()] = attrs;
+        }
+      }
+      return Object.keys(resultDict).map(key => resultDict[key]);
+    }
+  }
+
+  private static without(items: Attributes[], withoutItems: Attributes[], identifier: string): Attributes[] {
+    const resultDict: AttributesDict = {};
+    items.forEach(item => resultDict[item[identifier].toString()] = item);
+    withoutItems.forEach(item => delete resultDict[item[identifier].toString()]);
+    return Object.keys(resultDict).map(key => resultDict[key]);
   }
 
   private nextId(model: typeof NextModel): number {
@@ -131,7 +158,7 @@ export class DefaultConnector implements Connector {
     return this.ids[model.modelName]++;
   }
 
-  private indexOf(items: Attributes[], instance: NextModel): number | undefined {
+  private static indexOf(items: Attributes[], instance: NextModel): number | undefined {
     const identifier: string = instance.model.identifier;
     const id: any = instance[identifier];
     for (let index = 0; index < items.length; index++) {
@@ -142,19 +169,23 @@ export class DefaultConnector implements Connector {
     return undefined;
   }
 
+  private matchingItems(model: typeof NextModel): Attributes[] {
+    const items: Attributes[] = this.items(model);
+    return this.model.query(items, model.query, model.identifier);
+  }
+
   items(model: typeof NextModel): Attributes[] {
     this.storage[model.modelName] = this.storage[model.modelName] || [];
     return this.storage[model.modelName];
   }
 
   all(model: typeof NextModel): Promise<NextModel[]> {
-    const items: Attributes[] = this.items(model);
-    const attrArray: Attributes[] = this.query(items, model.query);
+    const attrArray: Attributes[] = this.matchingItems(model);
     return Promise.resolve(attrArray.map(attrs => new model(attrs)));
   }
 
   first(model: typeof NextModel): Promise<NextModel | undefined> {
-    const attrArray: Attributes[] = this.query(this.items(model), model.query);
+    const attrArray: Attributes[] = this.matchingItems(model);
     if (attrArray.length > 0) {
       return Promise.resolve(new model(attrArray[0]));
     } else {
@@ -163,22 +194,21 @@ export class DefaultConnector implements Connector {
   }
 
   count(model: typeof NextModel): Promise<number> {
-    return Promise.resolve(this.query(this.items(model), model.query).length);
+    return Promise.resolve(this.matchingItems(model).length);
   }
 
   updateAll(model: typeof NextModel, attrs: Attributes): Promise<NextModel[]> {
-    const items: Attributes[] = this.items(model);
-    const attrArray: Attributes[] = this.query(items, model.query);
+    const attrArray: Attributes[] = this.matchingItems(model);
     const instances = attrArray.map(attrs => new model(attrs));
     return Promise.all(instances.map(instance => this.update(instance.assign(attrs))));
   }
 
   deleteAll(model: typeof NextModel): Promise<NextModel[]> {
+    const attrArray: Attributes[] = this.matchingItems(model);
     const items: Attributes[] = this.items(model);
-    const attrArray: Attributes[] = this.query(items, model.query);
     const instances = attrArray.map(attrs => new model(attrs));
     instances.forEach(instance => {
-      const index: number | undefined = this.indexOf(this.items(model), instance);
+      const index: number | undefined = this.model.indexOf(items, instance);
       if (index !== undefined) {
         items.splice(index, 1);
       }
@@ -189,7 +219,7 @@ export class DefaultConnector implements Connector {
   reload(instance: NextModel): Promise<NextModel | undefined> {
     const model = instance.model;
     const items: Attributes[] = this.items(model);
-    const index: number | undefined = this.indexOf(items, instance);
+    const index: number | undefined = this.model.indexOf(items, instance);
     if (index === undefined) {
       return Promise.resolve(undefined);
     } else {
@@ -208,23 +238,25 @@ export class DefaultConnector implements Connector {
   update(instance: NextModel) {
     const model = instance.model;
     const items: Attributes[] = this.items(model);
-    const index: number | undefined = this.indexOf(items, instance);
+    const index: number | undefined = this.model.indexOf(items, instance);
     if (index !== undefined) {
       items[index] = instance.dbAttributes;
     }
     return Promise.resolve(instance);
   }
 
-
-
   delete(instance: NextModel) {
     const model = instance.model;
     const items: Attributes[] = this.items(model);
-    const index: number | undefined = this.indexOf(items, instance);
+    const index: number | undefined = this.model.indexOf(items, instance);
     if (index !== undefined) {
       items.splice(index, 1);
     }
     return Promise.resolve(instance);
+  }
+
+  get model(): typeof DefaultConnector {
+    return <typeof DefaultConnector>this.constructor;
   }
 };
 
