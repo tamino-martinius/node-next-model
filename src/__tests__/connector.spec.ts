342031831c09dc1c3fe702d449122dec307c6392
tamino-martinius <dev@zaku.eu>
2018-03-06 22:04:31 +0100

    Add specs for $lte

--- src/__tests__/connector.spec.ts
+++ src/__tests__/connector.spec.ts
@@ -1612,6 +1612,156 @@ describe('DefaultConnector', () => {
           });
         });
 
+        describe('$lte special filter', () => {
+          context('with empty filter', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<any> {
+                  return {
+                    $lte: {},
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('rejects filter and returns error', () => {
+                return expect(subject()).rejects.toEqual('[TODO] Return proper error');
+              });
+            },
+          });
+
+          context('with single filter within values', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<Identifiable> {
+                  return {
+                    $lte: {
+                      id: 2,
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('promises all matching items as model instances', () => {
+                return expect(subject()).resolves.toEqual([
+                  new Klass({ id: 1 }),
+                  new Klass({ id: 2 }),
+                ]);
+              });
+            },
+          });
+
+          context('with single filter with string attributes', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<any> {
+                  return {
+                    $lte: {
+                      foo: 'a',
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('promises empty array', () => {
+                return expect(subject()).resolves.toEqual([]);
+              });
+            },
+          });
+
+          context('with single filter with matching string attributes', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<any> {
+                  return {
+                    $lte: {
+                      foo: 'bar',
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('promises all matching items as model instances', () => {
+                return expect(subject()).resolves.toEqual([
+                  new Klass({ id: 1 }),
+                  new Klass({ id: 3 }),
+                ]);
+              });
+            },
+          });
+
+          context('with single filter going into range', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<Identifiable> {
+                  return {
+                    $lte: {
+                      id: 4,
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('promises all items as model instances', () => {
+                return expect(subject()).resolves.toEqual([
+                  new Klass({ id: 1 }),
+                  new Klass({ id: 2 }),
+                  new Klass({ id: 3 }),
+                ]);
+              });
+            },
+          });
+
+          context('with single filter outside', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<Identifiable> {
+                  return {
+                    $lte: {
+                      id: 0,
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('promises empty array', () => {
+                return expect(subject()).resolves.toEqual([]);
+              });
+            },
+          });
+
+          context('with multiple filters', {
+            definitions() {
+              class NewKlass extends Klass {
+                static get filter(): Filter<any> {
+                  return {
+                    $lte: {
+                      id: 2,
+                      foo: 'bar',
+                    },
+                  };
+                }
+              };
+              Klass = NewKlass;
+            },
+            tests() {
+              it('rejects filter and returns error', () => {
+                return expect(subject()).rejects.toEqual('[TODO] Return proper error');
+              });
+            },
+          });
+        });
       },
     });
   });
