acc4081c65106aa703c0c510199d966ab27891d8
tamino-martinius <dev@zaku.eu>
2018-03-14 19:06:27 +0100

    Attributes are not always complete

--- src/types.ts
+++ src/types.ts
@@ -182,7 +182,7 @@ export interface ModelStatic<S extends Identifiable> {
 export interface ModelConstructor<S extends Identifiable> {
   id: any;
   readonly model: ModelStatic<S>;
-  readonly attributes: S;
+  readonly attributes: Partial<S>;
   readonly persistentAttributes: Partial<S>;
   readonly isNew: boolean;
   readonly isPersistent: boolean;
