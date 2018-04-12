eee65a810c4288f79b7424e0c0c83e7c183b8ba4
tamino-martinius <dev@zaku.eu>
2018-04-12 19:43:54 +0200

    Add specs for #revertChanges()

--- src/__tests__/next_model.spec.ts
+++ src/__tests__/next_model.spec.ts
@@ -2452,7 +2452,41 @@ describe('NextModel', () => {
   });
 
   describe('#revertChanges()', () => {
-    pending('[TODO]');
+    let key: string = 'foo';
+    let attrs: any = { [key]: 'bar' };
+    let Klass: typeof Model = class NewKlass extends Faker.model {
+      static get schema() {
+        const schema = super.schema;
+        schema[key] = { type: 'foo' };
+        return schema;
+      }
+    };
+    let instance: ModelConstructor<any>;
+
+    const subject = () => instance.revertChanges().changes;
+
+    context('when instance is build', {
+      definitions() {
+        instance = Klass.build(attrs);
+      },
+      tests() {
+        it('returns attributes of instance', async () => {
+          expect(subject()).toEqual({});
+        });
+      },
+    });
+
+    context('when instance is created', {
+      async definitions() {
+        instance = await Klass.create(attrs);
+        attrs.id = instance.id;
+      },
+      tests() {
+        it('returns attributes of instance', async () => {
+          expect(subject()).toEqual({});
+        });
+      },
+    });
   });
   //#endregion
 
