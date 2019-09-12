3c370c8d15207d61950dbfe1486066ae062c79bd
Tamino Martinius <dev@zaku.eu>
2019-09-12 21:35:17 +0200

    Implicit set default export by order

--- src/Model.ts
+++ src/Model.ts
@@ -282,3 +282,5 @@ export function Model<
     }
   };
 }
+
+export default Model;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from './types';
 export * from './util';
-export * from './Model';
 export * from './MemoryConnector';
-
-import { Model } from './Model';
-export default Model;
+export * from './Model';
