15748191b84d6b6acb298e9133ceaa8f971985f9
Tamino Martinius <dev@zaku.eu>
2019-09-14 23:04:12 +0200

    Remove unused helpers

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,3 @@
-export function snakeToCamelCase(value: string) {
-  return value.replace(/_\w/g, m => m[1].toUpperCase());
-}
-
-export function camelToSnakeCase(value: string) {
-  return value.replace(/([A-Z])/g, m => `_${m.toLowerCase()}`);
-}
-
 export function uuid() {
   const dateStr = Date.now()
     .toString(16)
