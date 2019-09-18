8d44bb34211ea692c859c6c77c5465bc6da2bdeb
Tamino Martinius <dev@zaku.eu>
2019-09-18 14:42:20 +0200

    Preserve strong typing

--- src/Model.ts
+++ src/Model.ts
@@ -109,7 +109,7 @@ export class ModelClass {
   }
 
   static create<M extends typeof ModelClass>(this: M, createProps: Dict<any>) {
-    return this.build<M>(createProps).save() as InstanceType<M>;
+    return this.build<M>(createProps).save();
   }
 
   static createScoped<M extends typeof ModelClass>(this: M, props: Dict<any>) {
@@ -133,7 +133,7 @@ export class ModelClass {
     return items.pop();
   }
 
-  static async select(...keys: string[]) {
+  static async select(...keys: any[]) {
     const items = await this.connector.select(this.modelScope(), ...keys);
     return items;
   }
@@ -263,46 +263,14 @@ export function Model<
   const order = props.order ? (Array.isArray(props.order) ? props.order : [props.order]) : [];
   const keyDefinitions = props.keys || { id: KeyType.number };
 
-  return class ModelClass {
+  return class Model extends ModelClass {
     static tableName = props.tableName;
     static filter = props.filter;
     static limit = props.limit;
     static skip = props.skip;
     static order = order;
-
-    static modelScope() {
-      return {
-        tableName: this.tableName,
-        filter: this.filter,
-        limit: this.limit,
-        skip: this.skip,
-        order: this.order,
-      } as Scope;
-    }
-
-    static limitBy<M extends typeof ModelClass>(this: M, amount: number) {
-      return class extends (this as typeof ModelClass) {
-        static limit = amount;
-      } as M;
-    }
-
-    static unlimited<M extends typeof ModelClass>(this: M) {
-      return class extends (this as typeof ModelClass) {
-        static limit = undefined;
-      } as M;
-    }
-
-    static skipBy<M extends typeof ModelClass>(this: M, amount: number) {
-      return class extends (this as typeof ModelClass) {
-        static skip = amount;
-      } as M;
-    }
-
-    static unskipped<M extends typeof ModelClass>(this: M) {
-      return class extends (this as typeof ModelClass) {
-        static skip = undefined;
-      } as M;
-    }
+    static keys = keyDefinitions;
+    static connector = connector;
 
     static orderBy<M extends typeof ModelClass>(
       this: M,
@@ -310,18 +278,7 @@ export function Model<
         PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
       >,
     ) {
-      const newOrder = [...this.order, ...(Array.isArray(order) ? order : [order])];
-      return class extends (this as typeof ModelClass) {
-        static order = newOrder;
-      } as M;
-    }
-
-    static unordered<M extends typeof ModelClass>(this: M) {
-      return class extends (this as typeof ModelClass) {
-        static order: OrderColumn<
-          PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
-        >[] = [];
-      } as M;
+      return super.orderBy(order) as M;
     }
 
     static reorder<M extends typeof ModelClass>(
@@ -330,118 +287,71 @@ export function Model<
         PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
       >,
     ) {
-      return class extends (this as typeof ModelClass) {
-        static order = Array.isArray(order) ? order : [order];
-      } as M;
+      return super.reorder(order) as M;
     }
 
     static filterBy<M extends typeof ModelClass>(
       this: M,
-      andFilter: Filter<
+      filter: Filter<
         PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
       >,
     ) {
-      ///@ts-ignore
-      let filter:
-        | Filter<
-            PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
-          >
-        | undefined = andFilter;
-      if (this.filter) {
-        for (const key in this.filter) {
-          if ((this.filter as any)[key] !== undefined && (andFilter as any)[key] !== undefined) {
-            ///@ts-ignore
-            filter = { $and: [filter, andFilter] };
-            break;
-          }
-          (filter as any)[key] = (this.filter as any)[key];
-        }
-      }
-      ///@ts-ignore
-      if (Object.keys(andFilter).length === 0) filter = this.filter;
-      ///@ts-ignore
-      return class extends (this as typeof ModelClass) {
-        static filter = filter;
-      } as M;
+      return super.filterBy(filter) as M;
     }
 
     static orFilterBy<M extends typeof ModelClass>(
       this: M,
-      orFilter: Filter<
+      filter: Filter<
         PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
       >,
     ) {
-      const filter =
-        Object.keys(orFilter).length === 0
-          ? this.filter
-          : this.filter
-          ? { $or: [this.filter, orFilter] }
-          : orFilter;
-      ///@ts-ignore
-      return class extends (this as typeof ModelClass) {
-        static filter = filter;
-      } as M;
-    }
-
-    static unfiltered<M extends typeof ModelClass>(this: M) {
-      return class extends (this as typeof ModelClass) {
-        static filter = undefined;
-      } as M;
-    }
-
-    static build<M extends typeof ModelClass>(this: M, createProps: CreateProps) {
-      return new this(props.init(createProps)) as InstanceType<M> &
-        PersistentProps &
-        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
-    }
-
-    static buildScoped<M extends typeof ModelClass>(this: M, createProps: Partial<CreateProps>) {
-      return new this(
-        ///@ts-ignore
-        props.init({ ...props.filter, ...createProps } as CreateProps),
-      ) as InstanceType<M> &
-        PersistentProps &
-        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
-    }
-
-    static create<M extends typeof ModelClass>(this: M, props: CreateProps) {
-      return this.build<M>(props).save();
-    }
-
-    static createScoped<M extends typeof ModelClass>(this: M, props: Partial<CreateProps>) {
-      return this.buildScoped<M>(props).save();
-    }
-
-    static async all<M extends typeof ModelClass>(this: M) {
-      const items = (await connector.query(this.modelScope())) as (PersistentProps &
-        { [K in keyof Keys]: string })[];
-      return items.map(item => {
-        const keys = {} as { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number };
-        for (const key in keyDefinitions) {
-          keys[key as keyof Keys] = item[key];
-          delete item[key];
-        }
-        return new this(item, keys) as InstanceType<M> &
-          PersistentProps &
-          Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
-      });
-    }
-
-    static async first<M extends typeof ModelClass>(this: M) {
-      const items = await this.limitBy(1).all<M>();
-      return items.pop();
+      return super.orFilterBy(filter) as M;
     }
 
     static async select(...keys: [keyof Keys | keyof PersistentProps][]) {
-      const items = (await connector.select(this.modelScope(), ...(keys as any[]))) as Partial<
+      return (super.select(...(keys as any[])) as any) as Partial<
         PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }
       >[];
-      return items;
     }
 
     static async pluck(key: keyof Keys | keyof PersistentProps) {
-      const items = await this.select(key as any);
-      return items.map(item => item[key]);
+      return super.pluck(key as string);
+    }
+
+    static async all<M extends typeof ModelClass>(this: M) {
+      return (await super.all()) as (InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>)[];
+    }
+
+    static async first<M extends typeof ModelClass>(this: M) {
+      return (await super.first()) as InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
+    }
+
+    static build<M extends typeof ModelClass>(this: M, props: CreateProps) {
+      return super.build(props) as InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
+    }
+
+    static buildScoped<M extends typeof ModelClass>(this: M, props: Partial<CreateProps>) {
+      return super.buildScoped(props) as InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
+    }
+
+    static async create<M extends typeof ModelClass>(this: M, props: CreateProps) {
+      return (await super.create(props)) as InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
+    }
+
+    static async createScoped<M extends typeof ModelClass>(this: M, props: Partial<CreateProps>) {
+      return (await super.createScoped(props)) as InstanceType<M> &
+        PersistentProps &
+        Readonly<{ [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number }>;
     }
 
     persistentProps: PersistentProps;
@@ -452,34 +362,15 @@ export function Model<
       props: PersistentProps,
       keys?: { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number },
     ) {
-      this.persistentProps = props as PersistentProps;
-      this.keys = keys;
-
-      for (const key in this.persistentProps) {
-        Object.defineProperty(this, key, {
-          get: () => this.attributes[key],
-          set: value => this.assign({ [key]: value } as Partial<PersistentProps>),
-        });
-      }
-
-      for (const key in keyDefinitions) {
-        Object.defineProperty(this, key, {
-          get: () => (this.keys ? this.keys[key] : undefined),
-        });
-      }
-    }
-
-    get isPersistent() {
-      return this.keys !== undefined;
-    }
-
-    get isNew() {
-      return this.keys === undefined;
+      super(props, keys);
     }
 
     get attributes() {
-      ///@ts-ignore
-      return { ...this.persistentProps, ...this.changedProps, ...this.keys };
+      return {
+        ...(this.persistentProps as object),
+        ...(this.changedProps as object),
+        ...(this.keys as object),
+      } as PersistentProps & { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number };
     }
 
     assign<M extends ModelClass>(this: M, props: Partial<PersistentProps>) {
@@ -488,54 +379,6 @@ export function Model<
           this.changedProps[key] = props[key];
         } else {
           delete this.changedProps[key];
-        }
-      }
-      return this as M;
-    }
-
-    get itemScope(): Scope {
-      return {
-        tableName: props.tableName,
-        filter: this.keys,
-        limit: 1,
-        skip: 0,
-        order: [],
-      };
-    }
-
-    async save<M extends ModelClass>(this: M) {
-      if (this.keys) {
-        const changedKeys = Object.keys(this.changedProps);
-        if (changedKeys.length > 0) {
-          const items = await connector.updateAll(this.itemScope, this.changedProps);
-          const item = items.pop();
-          if (item) {
-            for (const key in keyDefinitions) {
-              this.keys[key as keyof Keys] = item[key];
-              delete item[key];
-            }
-            this.persistentProps = item as PersistentProps;
-            this.changedProps = {};
-          } else {
-            throw 'Item not found';
-          }
-        }
-      } else {
-        const items = await connector.batchInsert(props.tableName, keyDefinitions, [
-          ///@ts-ignore
-          { ...this.persistentProps, ...this.changedProps },
-        ]);
-        const item = items.pop();
-        if (item) {
-          this.keys = {} as { [K in keyof Keys]: Keys[K] extends KeyType.uuid ? string : number };
-          for (const key in keyDefinitions) {
-            this.keys[key as keyof Keys] = item[key];
-            delete item[key];
-          }
-          this.persistentProps = item as PersistentProps;
-          this.changedProps = {};
-        } else {
-          throw 'Failed to insert item';
         }
       }
       return this as M;
